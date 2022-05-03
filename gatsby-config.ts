c0fc912efb02d8a58f04c6c306dbc298caaf4f14
Alen Rubilar <lclc.alen@gmail.com>
2022-05-03 18:54:30 -0400

    :star Lorem :star

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,16 +3,24 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Without Mask Society`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "blog",
+        path: `${__dirname}/blog`,
+      },
+      // __key: "images"
     },
-    __key: "images"
-  }]
+    "gatsby-transformer-remark",
+  ],
 };
 
 export default config;
