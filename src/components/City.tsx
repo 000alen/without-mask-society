3a2449ab1ecfc0caf7591aafc1d36e773f86070c
Alen Rubilar <lclc.alen@gmail.com>
2022-05-04 23:58:56 -0400

    Lorem

--- src/components/City.tsx
+++ src/components/City.tsx
@@ -4,11 +4,57 @@ import React from "react";
 export const City = () => {
   return (
     <section>
-      <StaticImage
-        className="w-full h-auto"
-        src="../images/city/city.png"
-        alt=""
-      />
+      <div className="relative">
+        {/* group */}
+        <div>
+          {/* group-hover:scale-125 group-hover:drop-shadow-xl transition-all */}
+          <StaticImage
+            className="!absolute z-50 w-[34.3%] left-[5.9%] top-[60.2%]"
+            src="../images/city/P1.png"
+            alt=""
+          />
+
+          <StaticImage
+            className="!absolute z-50 w-[34.5%] left-[4.1%] top-[60.6%]"
+            src="../images/city/R1.png"
+            alt=""
+          />
+        </div>
+
+        <div className="group">
+          <StaticImage
+            className="!absolute peer z-50 w-[30.2%] left-[28.1%] top-[57.9%]"
+            src="../images/city/P2.png"
+            alt=""
+          />
+
+          <StaticImage
+            className="!absolute peer z-50 w-[30.1%] left-[28.4%] top-[67%]"
+            src="../images/city/R2.png"
+            alt=""
+          />
+        </div>
+
+        <div className="group">
+          <StaticImage
+            className="!absolute peer z-50 w-[31.2%] left-[54.3%] top-[59.8%]"
+            src="../images/city/P3.png"
+            alt=""
+          />
+
+          <StaticImage
+            className="!absolute peer z-50 w-[31.4%] left-[54.4%] top-[68.7%]"
+            src="../images/city/R3.png"
+            alt=""
+          />
+        </div>
+
+        <StaticImage
+          className="w-full h-auto"
+          src="../images/city/city.png"
+          alt=""
+        />
+      </div>
     </section>
   );
 };
--- src/components/Planets.tsx
+++ src/components/Planets.tsx
@@ -4,11 +4,38 @@ import React from "react";
 export const Planets = () => {
   return (
     <section>
-      <StaticImage
-        className="w-full h-auto"
-        src="../images/planets/planets.png"
-        alt=""
-      />
+      <div className="relative">
+        {/*  hover:scale-125 hover:drop-shadow-xl transition-all */}
+        <StaticImage
+          className="!absolute z-[48] w-[24.5%] left-[7.1%] top-[48.9%]"
+          src="../images/planets/P1.png"
+          alt=""
+        />
+
+        <StaticImage
+          className="!absolute z-[49] w-[29.9%] left-[29.5%] top-[47%]"
+          src="../images/planets/P2.png"
+          alt=""
+        />
+
+        <StaticImage
+          className="!absolute z-50 w-[40.1%] left-[50.5%] top-[46.9%]"
+          src="../images/planets/P3.png"
+          alt=""
+        />
+
+        <StaticImage
+          className="!absolute z-50 w-[87.7%] left-[4.7%] top-[7.1%]"
+          src="../images/planets/moons.png"
+          alt=""
+        />
+
+        <StaticImage
+          className="w-full h-auto"
+          src="../images/planets/planets.png"
+          alt=""
+        />
+      </div>
     </section>
   );
 };
