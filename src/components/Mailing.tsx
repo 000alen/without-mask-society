efe1a196738e63ae95c1b15c91ab4c68a94c02ba
Alen Rubilar <lclc.alen@gmail.com>
2022-05-05 20:57:58 -0400

    Lorem

--- src/components/Mailing.tsx
+++ src/components/Mailing.tsx
@@ -3,7 +3,7 @@ import { FancyButton } from "./FancyButton";
 
 export const Mailing = () => {
   return (
-    <div className="flex flex-col space-y-2 items-center lg:flex-row lg:space-x-4 lg:justify-center">
+    <div className="flex flex-col p-2 space-y-2 items-center lg:flex-row lg:space-x-4 lg:justify-center">
       <p className="max-w-lg text-center">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
         aspernatur libero sapiente nostrum eveniet qui deserunt.
