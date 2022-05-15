3b99468030ecc163229d5671e03cc2b9059935fa
Alen Rubilar <lclc.alen@gmail.com>
2022-05-15 14:46:36 -0400

    Lorem

--- src/components/RoadmapItem.tsx
+++ src/components/RoadmapItem.tsx
@@ -18,9 +18,13 @@ export const RoadmapItem: React.FC<RoadmapItemProps> = ({
     <div
       className={`timeline__event overflow-hidden text-ellipsis h-48 max-h-48 ${className}`}
     >
-      <div className="timeline__event__icon ">{icon}</div>
-      <div className="timeline__event__content ">
-        <div className="timeline__event__title">{title}</div>
+      <div className="bg-green-400 timeline__event__icon before:bg-green-400 after:bg-green-400">
+        {icon}
+      </div>
+      <div className="timeline__event__content">
+        <div className="text-[#e41388] font-[Aliseo] font-bold uppercase text-lg">
+          {title}
+        </div>
         <div className="timeline__event__description">
           <Markdown className="!text-black">{description}</Markdown>
         </div>
