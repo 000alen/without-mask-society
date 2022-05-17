8ab959cc373e7f631d15ae849677ab15d09c5a26
Alen Rubilar <lclc.alen@gmail.com>
2022-05-17 16:47:59 -0400

    Remove animations

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,73 +1,11 @@
-import React, { useEffect, useRef } from "react";
-import { lorem } from "../constants";
+import React from "react";
+
 import { BlogEntry } from "./BlogEntry";
-import { Question } from "./Question";
 import { Title } from "./Title";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/all";
-import { graphql, useStaticQuery } from "gatsby";
 import { useBlogEntries } from "./useBlogEntries";
-import { Footer } from "./Footer";
 
 export const Blog = () => {
   const blogEntries = useBlogEntries();
-
-  const itemsRef = useRef<Array<HTMLAnchorElement | null>>(
-    Array.from({ length: blogEntries.length }, () => null)
-  );
-
-  const hideItem = (item: HTMLAnchorElement) => {
-    gsap.set(item, { autoAlpha: 0 });
-  };
-
-  function animateItem(item: HTMLAnchorElement, direction: number = 1) {
-    let x = 0;
-    let y = direction * 100;
-
-    if (item.classList.contains("gs_fromLeft")) {
-      x = -300;
-      y = 0;
-    } else if (item.classList.contains("gs_fromRight")) {
-      x = 300;
-      y = 0;
-    }
-
-    item.style.transform = "translate(" + x + "px, " + y + "px)";
-    item.style.opacity = "0";
-
-    gsap.fromTo(
-      item,
-      { x: x, y: y, autoAlpha: 0 },
-      {
-        duration: 1.25,
-        x: 0,
-        y: 0,
-        autoAlpha: 1,
-        ease: "expo",
-        overwrite: "auto",
-      }
-    );
-  }
-
-  useEffect(() => {
-    itemsRef.current!.forEach((item) => {
-      hideItem(item!);
-
-      ScrollTrigger.create({
-        trigger: item,
-        onEnter: () => {
-          animateItem(item!);
-        },
-        onEnterBack: () => {
-          animateItem(item!, -1);
-          animateItem(item!, -1);
-        },
-        onLeave: () => {
-          hideItem(item!);
-        },
-      });
-    });
-  }, []);
 
   return (
     <section className="w-full" id="blog">
@@ -76,12 +14,7 @@ export const Blog = () => {
 
         <div className="flex flex-col gap-4">
           {blogEntries.map(({ title, date, path }, i) => (
-            <BlogEntry
-              key={i}
-              ref={(e) => (itemsRef.current[i] = e)}
-              title={title}
-              url={`/blog/${path}`}
-            />
+            <BlogEntry key={i} title={title} url={`/blog/${path}`} />
           ))}
         </div>
       </div>
--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -1,9 +1,8 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
+
 import { FAQQuestion } from "../typings";
 import { Question } from "./Question";
 import { Title } from "./Title";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/all";
 
 interface Props {
   faq_title: string;
@@ -11,63 +10,6 @@ interface Props {
 }
 
 export const FAQ: React.FC<Props> = ({ faq_title, faq_questions }) => {
-  const itemsRef = useRef<Array<HTMLDivElement | null>>(
-    Array.from({ length: faq_questions.length }, () => null)
-  );
-
-  const hideItem = (item: HTMLDivElement) => {
-    gsap.set(item, { autoAlpha: 0 });
-  };
-
-  function animateItem(item: HTMLDivElement, direction: number = 1) {
-    let x = 0;
-    let y = direction * 100;
-
-    if (item.classList.contains("gs_fromLeft")) {
-      x = -300;
-      y = 0;
-    } else if (item.classList.contains("gs_fromRight")) {
-      x = 300;
-      y = 0;
-    }
-
-    item.style.transform = "translate(" + x + "px, " + y + "px)";
-    item.style.opacity = "0";
-
-    gsap.fromTo(
-      item,
-      { x: x, y: y, autoAlpha: 0 },
-      {
-        duration: 1.25,
-        x: 0,
-        y: 0,
-        autoAlpha: 1,
-        ease: "expo",
-        overwrite: "auto",
-      }
-    );
-  }
-
-  useEffect(() => {
-    itemsRef.current!.forEach((item) => {
-      hideItem(item!);
-
-      ScrollTrigger.create({
-        trigger: item,
-        onEnter: () => {
-          animateItem(item!);
-        },
-        onEnterBack: () => {
-          animateItem(item!, -1);
-          animateItem(item!, -1);
-        },
-        onLeave: () => {
-          hideItem(item!);
-        },
-      });
-    });
-  }, []);
-
   return (
     <section className="w-full" id="faq">
       <div className="flex flex-col items-center gap-2">
@@ -76,12 +18,7 @@ export const FAQ: React.FC<Props> = ({ faq_title, faq_questions }) => {
         <div className="flex flex-col gap-4">
           {faq_questions &&
             faq_questions.map(({ faq_question, faq_answer }, i) => (
-              <Question
-                key={i}
-                ref={(e) => (itemsRef.current[i] = e)}
-                question={faq_question}
-                answer={faq_answer}
-              />
+              <Question key={i} question={faq_question} answer={faq_answer} />
             ))}
         </div>
       </div>
